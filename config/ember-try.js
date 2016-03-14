61db339730137808042128f67387d372209990bb
Mike North <michael.l.north@gmail.com>
2016-03-14 14:30:23 +0800

    Update ember-cli to 2.4.2

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,65 +1,65 @@
+/*jshint node:true*/
+function scenario(emberVersion) {
+  return {
+    name: 'ember-' + emberVersion,
+    bower: {
+      dependencies: {
+        'ember': '~' + emberVersion + '.0'
+      },
+      resolutions: {
+        'ember': '~' + emberVersion + '.0'
+      }
+    }
+  };
+}
+
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.11',
-      dependencies: {
-        'ember': '~1.11.0',
-        'ember-load-initializers': 'ember-cli/ember-load-initializers#0.0.2'
-      }
-    },
-    {
-      name: 'ember-1.12',
-      dependencies: {
-        'ember': '~1.12.0'
-      }
-    },
-    {
       name: 'default',
-      dependencies: {}
-    },
-    {
-      name: 'ember-1.13',
-      dependencies: {
-        'ember': '~1.13.0'
+      bower: {
+        dependencies: { }
       }
     },
-    {
-      name: 'ember-2.0',
-      dependencies: {
-        'ember': '~2.0.0'
-      }
-    },
-    {
-      name: 'ember-2.1',
-      dependencies: {
-        'ember': '~2.1.0'
-      }
-    },
+    scenario('1.10'),
+    scenario('1.11'),
+    scenario('1.12'),
+    scenario('1.13'),
+    scenario('2.0'),
+    scenario('2.1'),
+    scenario('2.2'),
+    scenario('2.3'),
     {
       name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
       }
     },
     {
       name: 'ember-beta',
-      dependencies: {
-        'ember': '2.0.0-beta.5'
-      },
-      resolutions: {
-        'ember': '2.0.0-beta.5'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
       }
     },
     {
       name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       }
     }
   ]

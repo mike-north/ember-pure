bebcc3cc28bac16432e1e584be06edc512326dec
Mike North <michael.l.north@gmail.com>
2017-05-24 02:53:09 +0200

    greenkeeper lockfile

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
   name: 'ember-pure',
 
-  included: function(app) {
-    var useContext = (app.options.pure && app.options.pure.useContext) ? app.options.pure.useContext : false;
-    var pureModules = app.options.pure && app.options.pure.modules
+  included(app) {
+    let useContext = (app.options.pure && app.options.pure.useContext) ? app.options.pure.useContext : false;
+    let pureModules = app.options.pure && app.options.pure.modules
       ? app.options.pure.modules
       : null;
     if (pureModules) {
-      var m = null;
-      app.import(app.bowerDirectory + '/pure/' + (useContext ? 'base-context' : 'base') + '.css');
+      let m = null;
+      app.import(`${app.bowerDirectory  }/pure/${  useContext ? 'base-context' : 'base'  }.css`);
       for (m in pureModules) {
-        app.import(app.bowerDirectory + '/pure/' + pureModules[m] + '.css');
+        app.import(`${app.bowerDirectory  }/pure/${  pureModules[m]  }.css`);
       }
     } else {
-      app.import(app.bowerDirectory + '/pure/pure.css');
-      app.import(app.bowerDirectory + '/pure/grids-responsive.css');
+      app.import(`${app.bowerDirectory  }/pure/pure.css`);
+      app.import(`${app.bowerDirectory  }/pure/grids-responsive.css`);
     }
   }
 };

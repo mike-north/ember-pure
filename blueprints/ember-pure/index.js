dea32e65f102376a0b18c1ef7500eeea13f3f3f0
Mike North <michael.l.north@gmail.com>
2018-11-03 22:10:38 -0700

    chore: fix linting errors

--- blueprints/ember-pure/index.js
+++ blueprints/ember-pure/index.js
@@ -4,10 +4,8 @@ var RSVP = require('rsvp');
 module.exports = {
   description: 'Setup ember-pure in an ember.js app',
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
-  beforeInstall: function (options) {
-    return RSVP.all([
-      this.addBowerPackageToProject('pure', '~0.6.0')
-    ]);
+  beforeInstall: function(/*options*/) {
+    return RSVP.all([this.addBowerPackageToProject('pure', '~0.6.0')]);
   }
 
   // locals: function(options) {
